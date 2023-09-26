3a606bceb07cebb5d90c628a8634fd06f190bef3
zhigibig <gamzat0004@mail.ru>
2023-09-26 11:31:12 +0300

    Update Professor model. Commented other models, because there is no need in them. Create example of Professor presenting subjects list.

--- my_dgunh_django/user/models.py
+++ my_dgunh_django/user/models.py
@@ -3,36 +3,38 @@ from django.contrib.auth.models import User
 from ..schedule.models import Group, Schedule
 
 
-class Student(models.Model):
+# class Student(models.Model):
 
-    class AuthorizationMethod(models.TextChoices):
+#     # class AuthorizationMethod(models.TextChoices):
 
-        DEFAULT = 'Default', 'Default Auth Method'
-        SBER_ID = 'Sber', 'Sber ID'
-        TINKOFF_ID = 'Tinkoff', 'Tinkoff ID'
-        YANDEX_ID = 'Ya', 'Yandex Pass'
-        GOOGLE_ID = 'Google', 'Google Mail'
-        VK_ID = 'VK', 'VK ID'
+#     #     DEFAULT = 'Default', 'Default Auth Method'
+#     #     SBER_ID = 'Sber', 'Sber ID'
+#     #     TINKOFF_ID = 'Tinkoff', 'Tinkoff ID'
+#     #     YANDEX_ID = 'Ya', 'Yandex Pass'
+#     #     GOOGLE_ID = 'Google', 'Google Mail'
+#     #     VK_ID = 'VK', 'VK ID'
 
 
-    GroupID = models.ForeignKey(Group, on_delete=models.CASCADE)
-    AuthMethod = models.CharField(
-        default=AuthorizationMethod.DEFAULT, 
-        choices=AuthorizationMethod.choices
-    )
-    UserName = models.CharField(max_length=32)
-    FirstName = models.CharField(max_length=64)
-    SecondName = models.CharField(max_length=64)
-    LastName = models.CharField(max_length=64)
-    isHead = models.BooleanField(default=False)
-    ChosenSchedule = models.ForeignKey(Schedule, on_delete=models.CASCADE, default=1)
+#     GroupID = models.ForeignKey(Group, on_delete=models.CASCADE)
+#     # AuthMethod = models.CharField(
+#     #     default=AuthorizationMethod.DEFAULT, 
+#     #     choices=AuthorizationMethod.choices
+#     # )
+#     # UserName = models.CharField(max_length=32)
+#     Email = models.EmailField(unique=True)
+#     FirstName = models.CharField(max_length=64)
+#     SecondName = models.CharField(max_length=64)
+#     LastName = models.CharField(max_length=64)
+#     # isHead = models.BooleanField(default=False)
+#     ChosenSchedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
 
 
 class Professor(models.Model):
+
     class DepartmentChoices(models.TextChoices):
         DEFAULT_DEPARTMENT = 'DEFAULT', 'DEFAULT DEPARTMENT'
 
-    Email = models.EmailField(blank=True)
+
     Department = models.CharField(
         max_length=128, 
         choices=DepartmentChoices
@@ -40,9 +42,7 @@ class Professor(models.Model):
     FirstName = models.CharField(max_length=64)
     SecondName = models.CharField(max_length=64)
     LastName = models.CharField(max_length=64)
-
-    # TODO: Make ScheduleviewName = SecondName + ' ' + FirstName[0] + '.' + LastName[0] + '.'
-    ScheduleviewName = models.CharField(max_length=64)
+    ScheduleViewName = models.CharField(max_length=64)
 
     # Here are subjects that professtor is presenting
     Subjects = models.JSONField()
@@ -51,10 +51,9 @@ class Professor(models.Model):
         unique_together = ('FirstName', 'SecondName', 'LastName', 'Subjects')
 
 
-class Administrator(models.Model):
+# class Administrator(models.Model):
 
-    UserName = models.CharField(max_length=32)
-    FirstName = models.CharField(max_length=64)
-    SecondName = models.CharField(max_length=64)
-    LastName = models.CharField(max_length=64)
-
+#     UserName = models.CharField(max_length=32)
+#     FirstName = models.CharField(max_length=64)
+#     SecondName = models.CharField(max_length=64)
+#     LastName = models.CharField(max_length=64)

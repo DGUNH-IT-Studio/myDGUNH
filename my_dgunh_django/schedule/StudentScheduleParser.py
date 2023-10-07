6cfff0d78eadf755b925f778a1f71450107782ce
zhigibig <gamzat0004@mail.ru>
2023-10-07 09:37:34 +0300

    Update scheduleParser.py.

--- my_dgunh_django/schedule/StudentScheduleParser.py
+++ my_dgunh_django/schedule/StudentScheduleParser.py
@@ -4,11 +4,18 @@ from professor_schedule.models import Department, Professor
 
 
 def main():
-    import json
-    all_schedules = Student_schedule.objects.all()
-    all_teachers = Professor.objects.all()
+    all_schedules = list(Student_schedule.objects.all())
+    all_teachers = list(Professor.objects.all())
 
-    
+    for i in all_teachers:
+        schedule_view_name = str(i.SecondName).capitalize() + ' ' + \
+                             str(i.FirstName)[0].upper() + '.' + \
+                             str(i.LastName)[0].upper() + '.'
+        lessons = list(i.Subjects)
+        for j in all_schedules:
+            current_schedule = dict(j.scheduleFile)
+            # написать перебор элементов расписания для поиска eventa который преподаватель преподает для занесения в расписание преподавателя
+            # написать класс для обработки вносимых в расписание преподавателя предметов
 
     return 0
 

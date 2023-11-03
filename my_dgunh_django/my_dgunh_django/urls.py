b97d9fad3112137858affea707af356b62fa530b
zhigibig <gamzat0004@mail.ru>
2023-11-03 09:49:20 +0300

    Add all models to serializers. Add all serializers to views. Add all ViewSets to urls. Complete first restful app.

--- my_dgunh_django/my_dgunh_django/urls.py
+++ my_dgunh_django/my_dgunh_django/urls.py
@@ -19,9 +19,50 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from schedule.views import ScheduleFormAPIview, StudentScheduleAPIview
+from schedule.views import *
+from rest_framework import routers
+
+
+BASE_ENDPOINT = 'api/v1/'
+
+
+term_router = routers.SimpleRouter()
+term_router.register(r'terms', TermViewSets)
+
+faculty_router = routers.SimpleRouter()
+faculty_router.register(r'faculties', FacultyViewSets)
+
+education_profile_router = routers.SimpleRouter()
+education_profile_router.register(r'eduprofiles', EducationProfileViewSets)
+
+education_program_router = routers.SimpleRouter()
+education_program_router.register(r'eduprograms', EducationProgramViewSets)
+
+university_group_router = routers.SimpleRouter()
+university_group_router.register(r'groups', UniversityGroupViewSets)
+
+student_schedule_router = routers.SimpleRouter()
+student_schedule_router.register(r'student_schedules', ScheduleViewSets)
+
+department_router = routers.SimpleRouter()
+department_router.register(r'departments', DepartmentViewSets)
+
+teacher_router = routers.SimpleRouter()
+teacher_router.register(r'teachers', TeacherViewSets)
+
+teacher_schedule_router = routers.SimpleRouter()
+teacher_schedule_router.register(r'teacher_schedules', TeacherScheduleViewSets)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(BASE_ENDPOINT, include(term_router.urls)),
+    path(BASE_ENDPOINT, include(faculty_router.urls)), # faculty
+    path(BASE_ENDPOINT, include(education_profile_router.urls)), # profile
+    path(BASE_ENDPOINT, include(education_program_router.urls)), # program
+    path(BASE_ENDPOINT, include(university_group_router.urls)), # group
+    path(BASE_ENDPOINT + 'schedule/', include(student_schedule_router.urls)),
+    path(BASE_ENDPOINT, include(department_router.urls)), # dep
+    path(BASE_ENDPOINT, include(teacher_router.urls)), # teacher
+    path(BASE_ENDPOINT + 'schedule/', include(teacher_schedule_router.urls)), # teacher schedule
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- my_dgunh_django/schedule/views.py
+++ my_dgunh_django/schedule/views.py
@@ -1,8 +1,52 @@
 from django.shortcuts import render, get_object_or_404
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import student_schedule
-from .serializers import StudentScheduleSerializer
+from .models import *
+from .serializers import *
 
+
+class TermViewSets(viewsets.ModelViewSet):
+    queryset = term.objects.all()
+    serializer_class = TermSerializer
+
+
+class FacultyViewSets(viewsets.ModelViewSet):
+    queryset = university_faculty.objects.all() 
+    serializer_class = FacultySerializer
+
+
+class EducationProfileViewSets(viewsets.ModelViewSet):
+    queryset = education_profile.objects.all()
+    serializer_class = EducationProfileSerializer
+
+
+class EducationProgramViewSets(viewsets.ModelViewSet):
+    queryset = education_program.objects.all()
+    serializer_class = EducationProgramSerializer
+
+
+class UniversityGroupViewSets(viewsets.ModelViewSet):
+    queryset = university_group.objects.all()
+    serializer_class = UniversityGroupSerializer
+
+
+class ScheduleViewSets(viewsets.ModelViewSet):
+    queryset = student_schedule.objects.all()
+    serializer_class = StudentScheduleSerializer
+
+
+class DepartmentViewSets(viewsets.ModelViewSet):
+    queryset = department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class TeacherViewSets(viewsets.ModelViewSet):
+    queryset = teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+
+class TeacherScheduleViewSets(viewsets.ModelViewSet):
+    queryset = teacher_schedule.objects.all()
+    serializer_class = TeacherScheduleSerializer
 
